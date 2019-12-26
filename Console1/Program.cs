c3199febd8903273cc2269dad6718a70789c68a9
RSatanovskyi <satcms@hotmail.com>
2019-12-26 12:52:28 +0200

    first commit

---
+++ Console1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Console1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://metanit.com/sharp/tutorial/2.1.php
+
+            int d = 6;
+            Console.WriteLine($"Hello World! d={d}");
+        }
+    }
+}
