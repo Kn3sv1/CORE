cfa54238620f3594796d133e923c6179a286def9
RSatanovskyi <satcms@hotmail.com>
2019-12-29 12:02:00 +0200

    added contravariant/covariant

---
+++ ConsoleCovariantInterface/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleCovariantInterface
+{
+    class Account
+    {
+        public virtual void DoTransfer(int sum)
+        {
+            Console.WriteLine($"Клиент положил на счет {sum} долларов");
+        }
+    }
+    class DepositAccount : Account
+    {
+        public override void DoTransfer(int sum)
+        {
+            Console.WriteLine($"Клиент положил на депозитный счет {sum} долларов");
+        }
+    }
+
+    //COVARIANT
+    interface IBank<out T>
+    {
+        T CreateAccount(int sum);
+    }
+
+    class Bank<T> : IBank<T> where T : Account, new()
+    {
+        public T CreateAccount(int sum)
+        {
+            T acc = new T();  // создаем счет
+            acc.DoTransfer(sum);
+            return acc;
+        }
+    }
+
+    //CONTRAVARIANT
+    interface ITransaction<in T>
+    {
+        void DoOperation(T account, int sum);
+    }
+
+    class Transaction<T> : ITransaction<T> where T : Account
+    {
+        public void DoOperation(T account, int sum)
+        {
+            account.DoTransfer(sum);
+        }
+    }
+
+    class Program
+    {
+
+        static void Covariant()
+        {
+            IBank<DepositAccount> depositBank = new Bank<DepositAccount>();
+            Account acc1 = depositBank.CreateAccount(34);
+
+            IBank<Account> ordinaryBank = new Bank<DepositAccount>();
+            // или так
+            // IBank<Account> ordinaryBank = depositBank;
+            Account acc2 = ordinaryBank.CreateAccount(45);
+        }
+
+        static void Contravariant()
+        {
+            ITransaction<Account> accTransaction = new Transaction<Account>();
+            accTransaction.DoOperation(new Account(), 400);
+
+            ITransaction<DepositAccount> depAccTransaction = new Transaction<Account>();
+            depAccTransaction.DoOperation(new DepositAccount(), 450);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            //https://metanit.com/sharp/tutorial/3.27.php
+            //Covariant();
+
+            Contravariant();
+
+            Console.Read();
+        }
+    }
+}
