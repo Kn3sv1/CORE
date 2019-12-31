fa5281cce1a419c649b104b0f3987ba8dcb3a16c
RSatanovskyi <satcms@hotmail.com>
2019-12-31 13:21:12 +0200

    Async

---
+++ Async/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    class Program
+    {
+        static void Factorial()
+        {
+            int result = 1;
+            for (int i = 1; i <= 6; i++)
+            {
+                result *= i;
+            }
+            Thread.Sleep(8000);
+            Console.WriteLine($"Факториал равен {result}");
+        }
+        // определение асинхронного метода
+        static async void FactorialAsync()
+        {
+            Console.WriteLine("Начало метода FactorialAsync"); // выполняется синхронно
+            await Task.Run(() => Factorial());                // выполняется асинхронно
+            Console.WriteLine("Конец метода FactorialAsync");
+        }
+
+        static void Main(string[] args)
+        {
+            //https://metanit.com/sharp/tutorial/13.3.php
+
+            Console.OutputEncoding = Encoding.UTF8;
+            FactorialAsync();   // вызов асинхронного метода
+
+            Console.WriteLine("Введите число: ");
+            int n = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"Квадрат числа равен {n * n}");
+
+            Console.WriteLine("Конец метода Main");
+            Console.Read();
+        }
+    }
+}
